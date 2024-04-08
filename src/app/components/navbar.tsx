1864ecaeff8564eb1d39ce26620f34b512e4aecc
Pierre Ferran <pierr3@users.noreply.github.com>
2024-04-08 18:26:55 -0400

    Base settings dialog

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "../style/navbar.scss";
 import Clock from "./clock";
+import SettingsModal from "./settingsmodal";
 
 const Navbar: React.FC = () => {
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
     <>
-    <div className="d-flex flex-md-row align-items-center p-3 px-md-4 mb-3 custom-navbar">
-      <Clock />
-      <span className="btn text-box-container m-2">Not Connected</span>
-      <a className="btn btn-info m-2 hide-connect-flex" href="#">Connect</a> 
-      <a className="btn btn-info m-2 hide-settings-flex" href="#">Settings</a>
-      {/* <span className="btn text-box-container m-2 fadeshow1">Datasource: Slurper</span> */}
-    </div>
+      <div className="d-flex flex-md-row align-items-center p-3 px-md-4 mb-3 custom-navbar">
+        <Clock />
+        <span className="btn text-box-container m-2">Not Connected</span>
+        <a className="btn btn-info m-2 hide-connect-flex" href="#">
+          Connect
+        </a>
+        <a
+          className="btn btn-info m-2 hide-settings-flex"
+          href="#"
+          onClick={() => setShowModal(true)}
+        >
+          Settings
+        </a>
+      </div>
+      { showModal && <SettingsModal closeModal={() => setShowModal(false)} /> }
     </>
   );
 };
