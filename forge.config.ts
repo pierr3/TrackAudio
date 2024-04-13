32735cd970ea4cc8b54dc8ea6f9585ffccbe4565
Pierre Ferran <pierr3@users.noreply.github.com>
2024-04-12 20:37:31 -0400

    Forge config for release (not working)

--- forge.config.ts
+++ forge.config.ts
@@ -7,9 +7,12 @@ import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import path from 'path';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
+import fs from 'fs';
+import { spawn } from 'child_process';
 
 const config: ForgeConfig = {
   packagerConfig: {
@@ -47,6 +50,68 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  hooks: {
+    packageAfterPrune: async (_, buildPath, __, platform) => {
+      const commands = [
+        "install",
+        "--no-package-lock",
+        "uiohook-napi",
+      ];
+
+      return new Promise((resolve, reject) => {
+        const oldPckgJson = path.join(buildPath, "package.json");
+        const newPckgJson = path.join(buildPath, "_package.json");
+
+        fs.renameSync(oldPckgJson, newPckgJson);
+
+        const npmInstall = spawn("npm", commands, {
+          cwd: buildPath,
+          stdio: "inherit",
+          shell: true,
+        });
+
+        npmInstall.on("close", (code) => {
+          if (code === 0) {
+            fs.renameSync(newPckgJson, oldPckgJson);
+
+            /**
+             * On windows code signing fails for ARM binaries etc.,
+             * we remove them here
+             */
+            if (platform === "win32") {
+              const problematicPaths = [
+                "darwin-x64",
+                "darwin-arm64",
+                "linux-arm64",
+                "linux-x64",
+              ];
+
+              problematicPaths.forEach((binaryFolder) => {
+                fs.rmSync(
+                  path.join(
+                    buildPath,
+                    "node_modules",
+                    "uiohook-napi",
+                    "prebuilds",
+                    binaryFolder
+                  ),
+                  { recursive: true, force: true }
+                );
+              });
+            }
+
+            resolve();
+          } else {
+            reject(new Error("process finished with error code " + code));
+          }
+        });
+
+        npmInstall.on("error", (error) => {
+          reject(error);
+        });
+      });
+    },
+  },
 };
 
 export default config;
--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,8 +1,8 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import path from 'path';
+// import path from 'path';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const CopyPlugin = require("copy-webpack-plugin");
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const CopyPlugin = require("copy-webpack-plugin");
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -11,12 +11,12 @@ export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
   }),
-  new CopyPlugin({
-    patterns: [
-      {
-        from: path.resolve(__dirname, "./node_modules/uiohook-napi/prebuilds"),
-        to: path.resolve(__dirname, "./dist/prebuilds")
-      }
-    ]
-  }),
+  // new CopyPlugin({
+  //   patterns: [
+  //     {
+  //       from: path.resolve(__dirname, "./node_modules/uiohook-napi/prebuilds"),
+  //       to: "node_modules/uiohook-napi/prebuilds"
+  //     }
+  //   ]
+  // }),
 ];
