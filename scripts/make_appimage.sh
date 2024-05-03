7fea0d005574eb73b52ad070368f3fccac82c07c
Pierre Ferran <pierr3@users.noreply.github.com>
2024-05-02 22:46:55 -0400

    Update node version to 20.x in CI workflows, AppImage bundling on linux

---
+++ scripts/make_appimage.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+arch=$1
+
+cp -R ./scrips/AppImage/TrackAudio.AppDir/ ./build/
+cp ./resources/*.wav ./build/TrackAudio.AppDir/usr/share/trackaudio/
+cp ./resources/LICENSE.txt ./build/TrackAudio.AppDir/usr/share/trackaudio/
+cp ./resources/AppIcon/AppIcon.png ./build/TrackAudio.AppDir/trackaudio.png
+cp ./resources/AppIcon/AppIcon.png ./build/TrackAudio.AppDir/.DirIcon
+cp ./resources/AppIcon/AppIcon.png ./build/TrackAudio.AppDir/usr/share/trackaudio/
+
+cp ./backend/build/Release/libafv_native.so ./build/TrackAudio.AppDir/usr/lib/
+chmod +x ./build/TrackAudio.AppDir/usr/lib/libafv_native.so
+
+cp -R ./out/TrackAudio-linux-$arch/** ./build/TrackAudio.AppDir/usr/share/
+chmod +x ./build/TrackAudio.AppDir/usr/share/trackaudio
+chmod 755 ./build/TrackAudio.AppDir/usr/share/trackaudio
+ln -s ./build/TrackAudio.AppDir/usr/share/trackaudio ./build/TrackAudio.AppDir/usr/bin/trackaudio 
+
+chmod +x ./build/TrackAudio.AppDir/trackaudio.desktop
+
+if [ "$arch" = "arm64" ]; then
+    mv ./build/TrackAudio.AppDir/AppRun-aarch64 ./build/TrackAudio.AppDir/AppRun
+    rm ./build/TrackAudio.AppDir/AppRun-x86_64
+else
+    mv ./build/TrackAudio.AppDir/AppRun-x86_64 ./build/TrackAudio.AppDir/AppRun
+    rm ./build/TrackAudio.AppDir/AppRun-aarch64
+fi
+
+chmod +x ./build/TrackAudio.AppDir/AppRun
+
+wget -O appimagetool-x86_64.AppImage https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
+
+./appimagetool-x86_64.AppImage ./build/TrackAudio.AppDir -u "gh-releases-zsync|pierr3|TrackAudio|latest|TrackAudio-*$arch.AppImage.zsync"
