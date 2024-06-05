aacc89853a0d03beaf39e902bac04ec2d516ccd6
Pierre Ferran <pierr3@users.noreply.github.com>
2024-06-04 20:15:07 -0400

    Bump version

--- backend/include/Shared.hpp
+++ backend/include/Shared.hpp
@@ -26,7 +26,7 @@
 
 #define API_SERVER_PORT 49080
 
-constexpr semver::version VERSION = semver::version { 1, 0, 2, semver::prerelease::beta, 7 };
+constexpr semver::version VERSION = semver::version { 1, 1, 0, semver::prerelease::beta, 8 };
 // NOLINTNEXTLINE
 const std::string CLIENT_NAME = std::string("TrackAudio-") + VERSION.to_string();
 
