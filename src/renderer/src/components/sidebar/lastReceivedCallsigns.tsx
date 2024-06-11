90329785b08d3c60dfc7b36b74d15268faef523e
Neil Enns <neile@live.com>
2024-06-11 13:54:15 -0700

    Address PR feedback

--- src/renderer/src/components/sidebar/lastReceivedCallsigns.tsx
+++ src/renderer/src/components/sidebar/lastReceivedCallsigns.tsx
@@ -1,24 +1,23 @@
 import { RadioType } from '@renderer/store/radioStore';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 type LastReceivedCallsignsProps = {
   radios: RadioType[];
 };
 
 const LastReceivedCallsigns: React.FC<LastReceivedCallsignsProps> = ({ radios }) => {
+  const rxRadios = useMemo(() => {
+    return radios.filter((radio) => radio.rx && radio.lastReceivedCallsign);
+  }, [radios]);
+
   return (
     <div className="box-container mt-3 w-100">
-      {' '}
-      <div style={{ textAlign: 'center' }} className="w-100 mb-0">
-        Last RX
-      </div>
-      {radios
-        .filter((radio) => radio.rx && radio.lastReceivedCallsign)
-        .map((radio, index) => (
-          <div key={index}>
-            {radio.callsign}: {radio.lastReceivedCallsign}
-          </div>
-        ))}
+      <div className="w-100 mb-0 text-center">Last RX</div>
+      {rxRadios.map((radio, index) => (
+        <div key={index}>
+          {radio.callsign}: {radio.lastReceivedCallsign}
+        </div>
+      ))}
     </div>
   );
 };
