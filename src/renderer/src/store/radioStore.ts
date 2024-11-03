3ae9662363991f386f7572949828f39866c17aef
George Barlow <george@barlw.uk>
2024-11-03 15:23:21 +0000

    HF alias commit

--- src/renderer/src/store/radioStore.ts
+++ src/renderer/src/store/radioStore.ts
@@ -3,10 +3,13 @@ import useSessionStore from './sessionStore';
 import { radioCompare } from '../helpers/RadioHelper';
 import { getCallsignParts } from '../helpers/CallsignHelper';
 import { GuardFrequency, UnicomFrequency } from '../../../shared/common';
+import { Station } from '@renderer/interfaces/Station';
 
 export interface RadioType {
   frequency: number;
+  frequencyAlias?: number;
   humanFrequency: string;
+  humanFrequencyAlias?: string;
   callsign: string;
   rx: boolean;
   tx: boolean;
@@ -37,6 +40,7 @@ interface RadioState {
   radios: RadioType[];
   radiosSelected: RadioType[];
   pttIsOn: boolean;
+  addRadioByStation: (radio: Station, stationCallsign: string) => void;
   addRadio: (frequency: number, callsign: string, stationCallsign: string) => void;
   removeRadio: (frequency: number) => void;
   setRadioState: (frequency: number, frequencyState: FrequencyState) => void;
@@ -99,6 +103,50 @@ const useRadioState = create<RadioState>((set, get) => ({
         ...state.radios,
         {
           frequency,
+          humanFrequency: RadioHelper.convertHzToMhzString(frequency),
+          callsign,
+          station,
+          position,
+          subPosition,
+          rx: false,
+          tx: false,
+          xc: false,
+          crossCoupleAcross: false,
+          currentlyTx: false,
+          currentlyRx: false,
+          onSpeaker: false,
+          selected: false,
+          transceiverCount: 0,
+          isPendingDeleting: false
+        }
+      ].sort((a, b) => radioCompare(a, b, stationCallsign))
+    }));
+  },
+  addRadioByStation: (radio: Station, stationCallsign: string) => {
+    const { frequency, name: callsign, frequencyAlias } = radio;
+
+    if (RadioHelper.doesRadioExist(useRadioState.getState().radios, frequency)) {
+      if (frequency !== UnicomFrequency && frequency !== GuardFrequency) {
+        postMessage(
+          'Frequency already exists in local client, but maybe not in AFV, delete it and try again'
+        );
+      }
+      return;
+    }
+
+    const [station, position, subPosition] = getCallsignParts(callsign);
+
+    const humanFrequencyAlias = frequencyAlias
+      ? RadioHelper.convertHzToMhzString(frequencyAlias)
+      : null;
+
+    set((state) => ({
+      radios: [
+        ...state.radios,
+        {
+          frequency,
+          frequencyAlias,
+          humanFrequencyAlias,
           humanFrequency: RadioHelper.convertHzToMhzString(frequency),
           callsign,
           station,
